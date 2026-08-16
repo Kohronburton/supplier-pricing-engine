de7376bc5390882cbf4a1989b9e49fff6d52ede6
Kohron Burton <allcryptoquick@gmail.com>
2026-08-16 19:27:50 -0400

    chore(v5): add interactive demo to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
+    {
+      url: "https://cpq.kohronburton.com/demo",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
   ];
 }
